e661ccfa0d56d1e13b4ca3b93b541a52188653ee
katie <kcmollhagen@gmail.com>
2018-10-25 15:31:04 -0400

    book class

---
+++ MidtermGroup/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermGroup
+{
+    class Book
+    {
+        private string author;
+        private string title;
+        bool checkedOut = false;
+        public Book(string author, string title)
+        {
+            this.author = author;
+            this.title = title;
+        }
+
+    }
+}
