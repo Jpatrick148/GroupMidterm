7bb58c2eacd79d240ab1bdb10e1351a06467a601
Jeremy Ryan <jscott257@gmail.com>
2018-10-26 13:19:00 -0400

    Adding App.cs for real this time

--- MidtermGroup/App.cs
+++ MidtermGroup/App.cs
@@ -8,5 +8,39 @@ namespace MidtermGroup
 {
     class App
     {
+        //edit to test
+        PrintMenu();
+        int menuPick; = ValidateInt();
+        
+        switch(menuPick)
+        {
+            case 1:
+            DisplayBooks();
+            break;
+            case 2:
+            PrintSearchMenu();
+            int searchMenuPick = ValidateInt();
+            switch(searchMenuPick)
+                {
+                    case 1:
+                    SearchAuthor();
+                    break;
+                    case 2:
+                    SearchTitle();
+                    break;
+                }
+
+            break;
+            
+            case 3:
+            CheckOutBook();
+            break
+            case 4:
+            ReturnBook();
+            break;
+            case 5:
+            Quit(); //Quit prints out the list using I/O
+        }
+
     }
 }
