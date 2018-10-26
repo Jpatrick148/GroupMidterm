58a4d6aff14d924a7766f5d966009ec8ce725882
Maria Filice <mfilice@williams-group.com>
2018-10-26 14:02:22 -0400

    Added Comment

--- MidtermGroup/Menu.cs
+++ MidtermGroup/Menu.cs
@@ -8,54 +8,26 @@ namespace MidtermGroup
 {
     class Menu
     {
-        
-        public static void MenuChoice()
+        public void PrintMenu()
         {
-
-            Console.WriteLine("1) List all books");
-            Console.WriteLine("2) Search by Author");
-            Console.WriteLine("3) Search by Title");
-            Console.WriteLine("4) Exit");
-            string choice = Console.ReadLine();
-
-
-            int pick = 0;
-            Int32.TryParse(choice, out pick);
-
-
-            {
-                switch (pick)
-                {
-
-
-                    case 1:
-                        //return
-                        break;
-
-                    case 2:
-                        //return
-                        break;
-
-                    case 3:
-                        //return
-                        break;
-
-                    case 4:
-                        //return
-                        break;
-
-
-                    default:
-                        //return
-                        break;
-
-
-                }
-            }
+            //menu options
+            Console.WriteLine("1- List all books");
+            Console.WriteLine("2- Search by Author ");
+            Console.WriteLine("3- Search by Title");
+            Console.WriteLine("4- Exit");
         }
-
+        public void PrintSearchOptions()
+        {
+            //comment
+            //search options 
+            Console.WriteLine("1- Enter an Author");
+            Console.WriteLine("2- Enter a Title ");
+        }
     }
 }
+
+
+     
 
 
     
